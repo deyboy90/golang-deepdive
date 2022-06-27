bd1bb70c075517b7e19040037a0f2bf5b2fb79be
Archit Dey <deyboy90@gmail.com>
2022-06-27 11:09:01 -0700

    Rename

--- dive1-composition/decoupling/run.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/deyboy90/golang-deepdive/dive1-composition/decoupling/style1"
-
-func main() {
-	style1.Execute()
-}
---
+++ dive2-composition/decoupling/run.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/deyboy90/golang-deepdive/dive2-composition/decoupling/style1"
+
+func main() {
+	style1.Execute()
+}
