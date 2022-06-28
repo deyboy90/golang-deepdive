efc2156977b24b721ac2bd94deab404ce59c95f7
Archit Dey <deyboy90@gmail.com>
2022-06-28 10:46:18 -0700

    Rename folders

--- dive2-composition/decoupling/run.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/deyboy90/golang-deepdive/dive2-composition/decoupling/style1"
-
-func main() {
-	style1.Execute()
-}
---
+++ dive4-composition/grouping/run.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/deyboy90/golang-deepdive/dive4-composition/grouping/behavior"
+	"github.com/deyboy90/golang-deepdive/dive4-composition/grouping/state"
+)
+
+func main() {
+	behavior.Execute()
+	state.Execute()
+}
